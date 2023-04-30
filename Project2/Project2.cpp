c6c20b01dcc9f7ddccf7c1b61fc36ac5ef3eef08
HirroKirr <kirillr89yandex.ru@mail.ru>
2023-04-30 14:07:19 +0300

    третий про3

---
+++ Project2/Project2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+/*
+типы данных:
+
+int - для работы с целыми числами.
+
+double - для работы с не целыми числами.
+
+char - хранит символы.
+
+bool - правда(true - 1),лож (false - 0).
+
+= - знак присваивания.
+
+*/
+
+
+
+
+
+void main()
+{
+	setlocale(LC_ALL, "ru");
+	int a = 8384;
+	cout << a << endl;
+	a = 4134;
+	cout << a << endl;
+}
