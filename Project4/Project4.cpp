03fadb8f16a73d3a143d06a2fdfd650bb22d2adb
HirroKirr <kirillr89yandex.ru@mail.ru>
2023-04-30 14:20:08 +0300

    описание

---
+++ Project4/Project4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+//Cin - ввод данных
+
+void main()
+{
+	setlocale(LC_ALL, "ru");
+	int var, var2;
+
+	cout << "введите 1 чесло" << endl;
+	cin >> var;
+
+	cout << "введите 2 чесло" << endl;
+	cin >> var2;
+
+	cout << "Число 1 =" << var << "\tЧисло 2 = " << var2 << endl;
+
+}
